e690de33b3bb561529f19b7c19ff5a976c993424
Ricardo MONLA <rmonla@gmail.com>
2020-06-23 20:06:19 -0300

    Update bdLinks.php

--- bdLinks.php
+++ bdLinks.php
@@ -8,9 +8,19 @@
 	$bdLnks = json_decode($bdLnks);
 
 
+
 	echo '<pre>'.print_r( $bdLnks, true ).'</pre>';
+
+	$bd2 = [
+		"bdCols"  => ["idZoom",      "idPlayList" ],
+		"1roICI-" => ["92023658063", "PLSOY8GuPHc0Cwf44jcSVV9t54r0ykksRV" ]
+	];
+
+	echo '<pre>'.print_r( $bd2, true ).'</pre>';
+
+
 	// echo $bdLnks->'bdCols';
-	echo $bdLnks->['bdCols'];
+	// echo $bdLnks->['bdCols'];
 	// echo $lnkDats->{'dticServTec'}
 	// echo ( isset( $bdLnks->{'dticServTec'} ))? "Existe" : "NoExiste";
 
