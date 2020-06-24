53ea244bd4cd2e948f90e1c6069168331b2eb007
Ricardo MONLA <rmonla@gmail.com>
2020-06-23 23:31:17 -0300

    Update index2.php

--- index2.php
+++ index2.php
@@ -55,7 +55,6 @@
                                 <?php echo getAula('4toICI-'); ?>
                                 <?php echo getAula('5toICI-'); ?>
                                 <?php echo getAula('6toICI-'); ?>
-                                
 
                               </div>
                           </div>
@@ -72,46 +71,12 @@
                         </div>
                         <div id="collapse2_0" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingTwo" data-parent="#bootstrap-accordion_0">
                             <div class="panel-body p-4">
-                                <p class="mbr-fonts-style panel-text display-7">
-                                    <a target="_blank" 
-                                    href="https://utn.zoom.us/j/91277544291"
-                                    >1roIEM-UTNLaRioja</a> | 
-                                    <a target="_blank" 
-                                    href="https://www.youtube.com/playlist?list=PLSOY8GuPHc0D1cIUQ6KE4Fa80VxzqlZGM"
-                                    >Clases_Grabadas</a>
-                                  </p>
-                                <p class="mbr-fonts-style panel-text display-7">
-                                    <a target="_blank" 
-                                    href="https://utn.zoom.us/j/92086937597"
-                                    >2doIEM-UTNLaRioja</a> | 
-                                    <a target="_blank" 
-                                    href="http://www.youtube.com/c/UTNLaRiojaDTIC/playlists"
-                                    >Clases_Grabadas</a>
-                                  </p>
-                                <p class="mbr-fonts-style panel-text display-7">
-                                    <a target="_blank" 
-                                    href="https://utn.zoom.us/j/92894001255"
-                                    >3roIEM-UTNLaRioja</a> | 
-                                    <a target="_blank" 
-                                    href="http://www.youtube.com/c/UTNLaRiojaDTIC/playlists"
-                                    >Clases_Grabadas</a>
-                                  </p>
-                                <p class="mbr-fonts-style panel-text display-7">
-                                    <a target="_blank" 
-                                    href="https://utn.zoom.us/j/92351175502"
-                                    >4toIEM-UTNLaRioja</a> | 
-                                    <a target="_blank" 
-                                    href="http://www.youtube.com/c/UTNLaRiojaDTIC/playlists"
-                                    >Clases_Grabadas</a>
-                                  </p>
-                                <p class="mbr-fonts-style panel-text display-7">
-                                    <a target="_blank" 
-                                    href="https://utn.zoom.us/j/96665531385"
-                                    >5toIEM-UTNLaRioja</a> | 
-                                    <a target="_blank" 
-                                    href="http://www.youtube.com/c/UTNLaRiojaDTIC/playlists"
-                                    >Clases_Grabadas</a>
-                                  </p>
+
+                                <?php echo getAula('1roIEM-'); ?>
+                                <?php echo getAula('2doIEM-'); ?>
+                                <?php echo getAula('3roIEM-'); ?>
+                                <?php echo getAula('4toIEM-'); ?>
+                                <?php echo getAula('5toIEM-'); ?>
                                 
                             </div>
                         </div>
@@ -127,54 +92,13 @@
                         </div>
                         <div id="collapse3_0" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#bootstrap-accordion_0">
                             <div class="panel-body p-4">
-                                <p class="mbr-fonts-style panel-text display-7">
-                                    <a target="_blank" 
-                                    href="https://utn.zoom.us/j/92116614010"
-                                    >1roIEL-UTNLaRioja</a> | 
-                                    <a target="_blank" 
-                                    href="http://www.youtube.com/c/UTNLaRiojaDTIC/playlists"
-                                    >Clases_Grabadas</a>
-                                  </p>
-                                <p class="mbr-fonts-style panel-text display-7">
-                                    <a target="_blank" 
-                                    href="https://utn.zoom.us/j/96130488690"
-                                    >2doIEL-UTNLaRioja</a> | 
-                                    <a target="_blank" 
-                                    href="http://www.youtube.com/c/UTNLaRiojaDTIC/playlists"
-                                    >Clases_Grabadas</a>
-                                  </p>
-                                <p class="mbr-fonts-style panel-text display-7">
-                                    <a target="_blank" 
-                                    href="https://utn.zoom.us/j/98516961454"
-                                    >3roIEL-UTNLaRioja</a> | 
-                                    <a target="_blank" 
-                                    href="https://www.youtube.com/playlist?list=PLSOY8GuPHc0BUcYc7vMS0tkRQDkEat9vv"
-                                    >Clases_Grabadas</a>
-                                  </p>
-                                <p class="mbr-fonts-style panel-text display-7">
-                                    <a target="_blank" 
-                                    href="https://utn.zoom.us/j/97174449400"
-                                    >4toIEL-UTNLaRioja</a> | 
-                                    <a target="_blank" 
-                                    href="http://www.youtube.com/c/UTNLaRiojaDTIC/playlists"
-                                    >Clases_Grabadas</a>
-                                  </p>
-                                <p class="mbr-fonts-style panel-text display-7">
-                                    <a target="_blank" 
-                                    href="https://utn.zoom.us/j/98672429951"
-                                    >5toIEL-UTNLaRioja</a> | 
-                                    <a target="_blank" 
-                                    href="http://www.youtube.com/c/UTNLaRiojaDTIC/playlists"
-                                    >Clases_Grabadas</a>
-                                  </p>
-                                <p class="mbr-fonts-style panel-text display-7">
-                                    <a target="_blank" 
-                                    href="https://utn.zoom.us/j/91937629997"
-                                    >6toIEL-UTNLarioja</a> | 
-                                    <a target="_blank" 
-                                    href="http://www.youtube.com/c/UTNLaRiojaDTIC/playlists"
-                                    >Clases_Grabadas</a>
-                                  </p>
+                                
+                                <?php echo getAula('1roIEL-'); ?>
+                                <?php echo getAula('2doIEL-'); ?>
+                                <?php echo getAula('3roIEL-'); ?>
+                                <?php echo getAula('4toIEL-'); ?>
+                                <?php echo getAula('5toIEL-'); ?>
+                                <?php echo getAula('6toIEL-'); ?>
 
                             </div>
                         </div>
@@ -190,11 +114,9 @@
                         </div>
                         <div id="collapse4_0" class="panel-collapse noScroll collapse" role="tabpanel" aria-labelledby="headingThree" data-parent="#bootstrap-accordion_0">
                             <div class="panel-body p-4">
-                                <p class="mbr-fonts-style panel-text display-7">
-                                    <a target="_blank" 
-                                    href="https://zoom.us/j/435997049"
-                                    >Aula Pública - UTNLaRioja</a>
-                                 </p>
+
+                                <?php echo getAula('Aula Pública-'); ?>
+
                             </div>
                         </div>
 
