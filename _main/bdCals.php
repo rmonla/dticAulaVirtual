232ddcd6417190a7cf081d577c77ce435fa7efc7
Ricardo MONLA <rmonla@gmail.com>
2020-08-20 22:12:58 -0300

    Update bdCals.php

--- _main/bdCals.php
+++ _main/bdCals.php
@@ -2,6 +2,12 @@
 	// Base de datos de Calendarios.
 		$bdCal = [
 			'Cols'       => 'idGoogleCal',
+			'ICI-1ro'    => 'tolk7hodqmgn7trhohtqfjgbn8',
+			'ICI-2do'    => 'v424gp9t35n8k89r5hgk4op72s',
+			'ICI-3ro'    => 'hsqlqftojq9qphv225rc10hk98',
+			'ICI-4to'    => 'hqvfb691ua6irogbg4ug56o388',
+			'ICI-5to'    => 'ps62t3hlmljku5ogtti3sct478',
+			'ICI-6to'    => 'p41km38omaubbc1t2ssl8ik2v8',
 			'PREINGRESO' => '90dlmf0j5c9a5h09jkhbojcuss',
 			'EVENTOS'    => 'khar5qjvem7k27pek22ldqbkgg',
 			'CATERM'     => 'ntb633871fphuvn3fmbphhgr4s',
