dde955a07df0bbf93634691eb8f9d9b7ecd7808a
Ricardo MONLA <rmonla@gmail.com>
2020-08-25 17:23:24 -0300

    Update index.php

--- final/0828/index.php
+++ final/0828/index.php
@@ -6,6 +6,8 @@
 	$titu = "Final | Proyecto Final | ICI 6to [_28deAgosto-18Hs_]";
 	$desc = "Acceso al Aula Virtual";
 	
+	$urlGenialy = "https://view.genial.ly/5f4474b2a629680d72e9e2be";
+	
 	$imag = URL_IMG."final.png";
 
 	$time = 60;
@@ -28,7 +30,7 @@
 				<center>
 					<div style="width: 90%;">
 						<div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;">
-							<iframe frameborder="0" width="850px" height="900px" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://view.genial.ly/5f185b91c43a880d757f18fc" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe>
+							<iframe frameborder="0" width="850px" height="900px" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="$urlGenialy" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe>
 						</div>
 					</div>	
 				</center>
