2d78ccd703055ff724f89956a55b086571c0f440
Ricardo MONLA <rmonla@gmail.com>
2020-08-26 21:57:12 -0300

    Centro el iframe.

--- laygies/index.php
+++ laygies/index.php
@@ -8,32 +8,10 @@
 	
 	$imag = URL_IMG."laygies.jpg";
 
-	$time = 30;
+	$time = 1800;
 	$dest = "../laygies";
 
-	/**
-	 * Hoy comenzamos con el cursado de la licenciatura Nodocente desde las 14.
-Este semestre tendremos 4 asignaturas.
-Se organizará de la siguiente manera:
-
-Diseño y Gestión en Proyectos y Programas en IES 
-[Lun de 14 a 16 Hs.] Claudia RESTTIFO
-
-Seguridad y Salud Laboral - Espacio Electivo 
-[Mié de 14 a 15:30 Hs.] Johanna MENDEZ
-
-Legislación y Procedimientos Administrativos 
-[Mié de 15:30 a 17 Hs.] Silvia IANCHINA
-
-Metodología de la Investigación 
-[Vie de 14 a 16 Hs.] Víctor Oliva
-
-*Miércoles de 14 a 15.30 Johanna Mendez: Espacio Electivo- 
-
-*Miércoles de 15.30 a 17 Silvia Ianchina: 
-
-*Viernes de 14 a 16 Víctor Oliva: Metodología de la Investigación
-	 */
+	
 
 	echo <<<HTML
 
@@ -46,13 +24,15 @@ Metodología de la Investigación
 			<meta http-equiv="Refresh" content="$time;url=$dest"/>
 		</head>
 		<body>
-			<p>
-				<div style="width: 90%;">
-					<div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;">
-						<iframe frameborder="0" width="850px" height="900px" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://view.genial.ly/5f3ff198089eb30d797127c9" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe>
-					</div>
-				</div>	
-			</p>
+			<center>
+				<p>
+					<div style="width: 90%;">
+						<div style="position: relative; padding-bottom: 56.25%; padding-top: 0; height: 0;">
+							<iframe frameborder="0" width="850px" height="900px" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="https://view.genial.ly/5f3ff198089eb30d797127c9" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe>
+						</div>
+					</div>	
+				</p>
+			</center>
 		</body>
 	</html>		
 
