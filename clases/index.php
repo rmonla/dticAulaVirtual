934223dd4cf275cf28255514d87228045ca8f754
Ricardo MONLA <rmonla@gmail.com>
2020-09-14 12:50:48 -0300

    Actu 200914-1250

---
+++ clases/index.php
@@ -0,0 +1,6 @@
+<?php
+	// http://www.frlr.utn.edu.ar/dtic/aulavirtual/clases
+	$newURL = 'https://www.youtube.com/c/UTNLaRiojaDTIC';
+	header('Location: '.$newURL);
+	die();
+?>
---
+++ cuentas/index.php
@@ -0,0 +1,6 @@
+<?php
+	// http://www.frlr.utn.edu.ar/dtic/aulavirtual/cuentas
+	$newURL = 'https://docs.google.com/spreadsheets/d/1xqRg0KEbhj23oIk75YuNc-UEXh08AnYWssBtTNMv3c0';
+	header('Location: '.$newURL);
+	die();
+?>
