e95dfa74118ae0d221b8da1df74c48e6f44f1ee2
Ricardo MONLA <rmonla@gmail.com>
2020-09-15 22:44:25 -0300

    Agrega Recursos

---
+++ charlas/ExpoFUTURO/recursos/index.php
@@ -0,0 +1,47 @@
+<?php 
+	header('Content-Type: text/html; charset=utf-8');
+	define('SITIO', 'http://www.frlr.utn.edu.ar/dtic/aulavirtual/charlas/ExpoFUTURO/');
+	define('URL', SITIO.'contactos/');
+	
+	$titu = "RECURSOS [ExpoFuturo2020]";
+	$desc = "Presentaciones, Videos, Guiones, etc.";
+	
+	$imag = URL."recursos.png";
+
+	$time = 30;
+	$dest = "https://drive.google.com/drive/folders/1UaKCMzP2p-CtoYjO6iYYRFnbHEQQpq7O";
+
+	$idspreadsheets = "2PACX-1vS81-oewP8sQA3B9lHJeMj_pYxDCu3LkqWbX5cIkt1rjlCfWdfPjjc6uHiQ_1gXnBSgVbhs1-Bi_PJ5";
+
+	//URL Acceso. 
+	//  http://www.frlr.utn.edu.ar/dtic/aulavirtual/charlas/ExpoFUTURO/contactos?1
+
+	//Acceso al archivo del drive. 
+	//https://docs.google.com/spreadsheets/d/1fhDi2_Ax-IgIZUpXLmBiRk0IQ-o9k1JhqtaKRCGfM9E
+
+	echo <<<HTML
+
+	<html>
+		<head>
+			<title>$titu</title>
+			<meta name="description"   content="$desc">
+			<meta property="og:title"  content="$titu">
+			<meta property="og:image"  content="$imag">
+			<meta http-equiv="Refresh" content="$time;url=$dest"/>
+		</head>
+		<body>
+			<center>
+				<p>
+					Redirección automática en $time segundos o puedes acceder haciendo click <a href="$dest">aquí</a>
+				</p>
+				<p>
+					<a href="$dest"><img src="$imag" alt="$desc" width="400"></a>
+				</p>
+			</center>
+		</body>
+	</html>		
+
+HTML;
+
+?>
+
