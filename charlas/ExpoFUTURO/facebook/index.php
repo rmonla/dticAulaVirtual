c09bcd572d385fa95fd72c12a18a8fbac4e0791e
Ricardo MONLA <rmonla@gmail.com>
2020-09-22 18:27:41 -0300

    Update index.php

--- charlas/ExpoFUTURO/facebook/index.php
+++ charlas/ExpoFUTURO/facebook/index.php
@@ -1,6 +1,7 @@
 <?php
 	// http://www.frlr.utn.edu.ar/dtic/aulavirtual/cuentas
-	$newURL = '../';
+	$newURL = 'https://www.facebook.com/100000245235383/videos/4966912613326864/';
+	// $newURL = '../';
 	header('Location: '.$newURL);
 	die();
 ?>
