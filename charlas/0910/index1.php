9e3e066e7bf7b5c00466a6327c60c2592ad27f58
Ricardo MONLA <rmonla@gmail.com>
2020-09-23 16:34:48 -0300

    Actu200923-1634

---
+++ charlas/0910/index1.php
@@ -0,0 +1,45 @@
+<?php 
+	// define('URL', '../../');
+	define('URL', 'http://www.frlr.utn.edu.ar/dtic/aulavirtual/charlas/0910/');
+	
+	$titu = "ExpoFuturo2020 | UTNLaRioja";
+	$desc = "Acceso a las Charlas [Inicia 10 Septiembre 18 Hs.]";
+	
+	$genialyID = "5f59560e60ac360d7b3fb8ec";
+	
+	$genialyURL = "https://view.genial.ly/" . $genialyID;
+	
+	$imag = URL."imag2.png";
+
+	$time = 300;
+	$dest = "../0910";
+
+	// http://www.frlr.utn.edu.ar/dtic/aulavirtual/charlas/0910?1
+
+	echo <<<HTML
+
+	<html>
+		<head>
+			<title>$titu</title>
+			<meta name="description"   content="$desc">
+			<meta property="og:title"  content="$titu">
+			<meta property="og:image"  content="$imag">
+			<meta http-equiv="Refresh" content="$time;url=$dest"/>
+		</head>
+		<body>
+			<center>
+				<p>
+					<div">
+						<div >
+							<iframe frameborder="0" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="$genialyURL" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe>
+						</div>
+					</div>	
+				</p>
+			</center>
+		</body>
+	</html>		
+
+HTML;
+
+?>
+
