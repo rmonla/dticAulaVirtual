0cf8426cbfc997befc5f8845c6602d6d4a46526e
Ricardo MONLA <rmonla@gmail.com>
2020-09-24 19:09:57 -0300

    Update index.php

--- charlas/ExpoFUTURO/facebook/index.php
+++ charlas/ExpoFUTURO/facebook/index.php
@@ -1,5 +1,6 @@
 <?php
-	$newURL = 'https://www.facebook.com/watch/?v=358740285320811';
+	$newURL = 'https://www.facebook.com/watch/?v=245981446813552';
+	// $newURL = 'https://www.facebook.com/watch/?v=358740285320811';
 	// $newURL = 'https://www.facebook.com/educacionlar/';
 	// $newURL = 'https://www.facebook.com/watch/?v=657941488433929';
 	// $newURL = 'https://www.facebook.com/watch/?v=1055919151519291';
