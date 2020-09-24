026a89991344eaa08805f3c25c462ceaddccd8d3
Ricardo MONLA <rmonla@gmail.com>
2020-09-24 19:09:49 -0300

    Update index.php

--- charlas/ExpoFUTURO/youtube/index.php
+++ charlas/ExpoFUTURO/youtube/index.php
@@ -1,5 +1,6 @@
 <?php
-	$newURL = 'https://youtu.be/sPilnF_ZTgc';
+	$newURL = 'https://www.youtube.com/watch?v=WI_lRW3874g';
+	// $newURL = 'https://youtu.be/sPilnF_ZTgc';
 	// $newURL = 'https://www.youtube.com/user/educacionlarioja';
 	// $newURL = 'https://youtu.be/CYhxWYDMf1g';
 	// $newURL = 'https://youtu.be/3IwQwucCMfQ';
