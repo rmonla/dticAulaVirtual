66f35481c8b0c6ab0c58d7268a9247f45f1f0c40
Ricardo MONLA <rmonla@gmail.com>
2020-09-24 09:54:13 -0300

    Actu200924-0952

--- charlas/ExpoFUTURO/facebook/index.php
+++ charlas/ExpoFUTURO/facebook/index.php
@@ -1,7 +1,7 @@
 <?php
-	$newURL = 'https://www.facebook.com/watch/?v=657941488433929';
+	$newURL = 'https://www.facebook.com/educacionlar/';
+	// $newURL = 'https://www.facebook.com/watch/?v=657941488433929';
 	// $newURL = 'https://www.facebook.com/watch/?v=1055919151519291';
-	// $newURL = 'https://www.facebook.com/educacionlar/videos';
 	// $newURL = 'https://www.facebook.com/watch/?v=697265257533899';
 	// $newURL = 'https://www.facebook.com/100000245235383/videos/4966912613326864/';
 	// $newURL = '../';
--- charlas/ExpoFUTURO/youtube/index.php
+++ charlas/ExpoFUTURO/youtube/index.php
@@ -1,7 +1,7 @@
 <?php
-	$newURL = 'https://youtu.be/CYhxWYDMf1g';
+	$newURL = 'https://www.youtube.com/user/educacionlarioja';
+	// $newURL = 'https://youtu.be/CYhxWYDMf1g';
 	// $newURL = 'https://youtu.be/3IwQwucCMfQ';
-	// $newURL = 'https://www.youtube.com/user/educacionlarioja/featured';
 	// $newURL = 'https://youtu.be/QsnDeL2MBJg';
 	header('Location: '.$newURL);
 	die();
