1fae9357139081e19dc801ba5a80cedbe8cc1e6b
Ricardo MONLA <rmonla@gmail.com>
2020-09-30 03:52:26 -0300

    Actu200930-0352

---
+++ curso/1005impresion3D/youtube/index.php
@@ -0,0 +1,6 @@
+<?php
+	// http://www.frlr.utn.edu.ar/dtic/aulavirtual/cuentas
+	$newURL = 'https://utn.zoom.us/j/97650224441';
+	header('Location: '.$newURL);
+	die();
+?>
---
+++ curso/1005impresion3D/zoom/index.php
@@ -0,0 +1,6 @@
+<?php
+	$newURL = 'https://utn.zoom.us/j/83445252626';
+	// $newURL = 'https://utn.zoom.us/j/97650224441';
+	header('Location: '.$newURL);
+	die();
+?>
