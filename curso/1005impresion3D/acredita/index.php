4b086b4de0c5bf95172dd67787209ece995f4312
Ricardo MONLA <rmonla@gmail.com>
2020-10-05 09:43:06 -0300

    Update index.php

--- curso/1005impresion3D/acredita/index.php
+++ curso/1005impresion3D/acredita/index.php
@@ -1,5 +1,5 @@
 <?php
-	$newURL = 'https://utn.zoom.us/j/97650224441';
+	$newURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSE9MrfPmN0nDmgO_5bxeGdcfWnL4GU_n7BY0DNCWb86JTqnVuLIz941Da4aktL_A6ZVQB58n6_s7Yh/pubhtml?gid=1639096385&single=true';
 	header('Location: '.$newURL);
 	die();
 ?>
