b3262662dec1fb7e04186a24968a2564f9eb625e
Ricardo MONLA <rmonla@gmail.com>
2020-10-05 10:37:55 -0300

    Update index.php

--- curso/1005impresion3D/youtube/index.php
+++ curso/1005impresion3D/youtube/index.php
@@ -1,5 +1,5 @@
 <?php
-	$newURL = 'https://utn.zoom.us/j/97650224441';
+	$newURL = 'https://www.youtube.com/playlist?list=PLSOY8GuPHc0B5dH_FdDmI6z-jzIKKn0lU';
 	header('Location: '.$newURL);
 	die();
 ?>
