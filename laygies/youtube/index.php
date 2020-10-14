f8ba297ae2c81e5d040e6aefd4a6f8de1516a4c8
Ricardo MONLA <rmonla@gmail.com>
2020-10-14 18:25:39 -0300

    Actu201014-1825

---
+++ laygies/youtube/index.php
@@ -0,0 +1,7 @@
+<?php
+	$newURL = 'https://www.youtube.com/playlist?list=PLSOY8GuPHc0DQXCppXPDtsk_il_lA65YG';
+	header('Location: '.$newURL);
+	die();
+
+	// http://www.frlr.utn.edu.ar/dtic/aulavirtual/finales/201009ICI/youtube
+?>
