b4d57e3376862fee8cefe60ce368e4225919e3c9
Ricardo MONLA <rmonla@gmail.com>
2020-10-15 18:35:09 -0300

    Actu201015-1834

---
+++ laygies/campus/index.php
@@ -0,0 +1,7 @@
+<?php
+	$newURL = 'https://www.campusvirtual.frm.utn.edu.ar/my/';
+	header('Location: '.$newURL);
+	die();
+
+	// http://www.frlr.utn.edu.ar/dtic/aulavirtual/finales/201009ICI/youtube
+?>
