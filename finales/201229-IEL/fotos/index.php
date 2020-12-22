3bb3a4402e92d0e8b4e12b657eaf7d20d3454cea
Ricardo MONLA <rmonla@gmail.com>
2020-12-22 18:36:50 -0300

    final-201229-IEL

---
+++ finales/201229-IEL/fotos/index.php
@@ -0,0 +1,6 @@
+<?php
+	$newURL = 'https://photos.app.goo.gl/BPQ7RhooLjrbHVkv5';
+	header('Location: '.$newURL);
+	die();
+
+?>
---
+++ finales/201229-IEL/youtube/index.php
@@ -0,0 +1,7 @@
+<?php
+	$newURL = 'https://www.youtube.com/playlist?list=PLSOY8GuPHc0DrNUOa2-UU5c4sYjdRfeel';
+	header('Location: '.$newURL);
+	die();
+
+	// http://www.frlr.utn.edu.ar/dtic/aulavirtual/finales/201127-LicHyS/youtube
+?>
