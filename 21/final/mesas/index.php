7170c3f667b73e69832780c19e6e7e1991dce091
Ricardo MONLA <rmonla@gmail.com>
2021-01-29 19:09:05 -0300

    Create index.php

---
+++ 21/final/mesas/index.php
@@ -0,0 +1,9 @@
+<?php
+	
+	$newURL = 'https://calendar.google.com/calendar/embed?src=idjvpem28ibjhhfbis764jns7k%40group.calendar.google.com&ctz=America%2FArgentina%2FBuenos_Aires';
+	
+	header('Location: '.$newURL);
+
+	die();
+
+?>
