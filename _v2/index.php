42fb23b9e083c9bcc41be3388babb7522427c6d9
Ricardo MONLA <rmonla@gmail.com>
2021-02-16 19:42:26 -0300

    INiciando versión 2.x

---
+++ _v2/index.php
@@ -0,0 +1,19 @@
+<?php  
+/**
+ * dticAulaVirtual es el servicio de aulas virtuales de la UTNLaRioja.
+ * El objetivo principa es el de dar accesos a los distintos integrantes
+ * de a comunidad educativas a las clases.
+ * 
+ * @license http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+define('APPVER', '2.21.1');
+define('APPNOM', 'dticAulaVirtual');
+define('APPAUT', 'Ricardo MONLA <rmonla@gmail.com>');
+define('APPLNKDEV', 'https://github.com/rmonla/dticAulaVirtual');
+define('APPLNKPUB', 'http://www.frlr.utn.edu.ar/dtic/aulavirtual');
+
+
+
+
+?>
