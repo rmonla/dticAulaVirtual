fd145c2ed95745d5becec5a6f548f32ef62587a1
Ricardo MONLA <rmonla@gmail.com>
2021-02-18 11:33:38 -0300

    Defino cabecera de APP.

--- _v2/index.php
+++ _v2/index.php
@@ -1,19 +1,25 @@
 <?php  
-/**
- * dticAulaVirtual es el servicio de aulas virtuales de la UTNLaRioja.
- * El objetivo principa es el de dar accesos a los distintos integrantes
- * de a comunidad educativas a las clases.
- * 
- * @license http://www.opensource.org/licenses/mit-license.php MIT License
- */
 
-define('APPVER', '2.21.1');
-define('APPNOM', 'dticAulaVirtual');
-define('APPAUT', 'Ricardo MONLA <rmonla@gmail.com>');
-define('APPLNKDEV', 'https://github.com/rmonla/dticAulaVirtual');
-define('APPLNKPUB', 'http://www.frlr.utn.edu.ar/dtic/aulavirtual');
+	define('APPVER', '2.21.1');
+	define('APPNOM', 'dticAulaVirtual');
+
+	define('APPDET', '
+		
+		## Servicio de aulas virtuales de la UTNLaRioja. ## 
+		El objetivo principal es el de dar acceso a las clases no presenciales a los distintos integrantes de la comunidad educativa de la Facultad.
+
+		');
+
+	define('APPAUT', 'Ricardo MONLA <rmonla@gmail.com>');
+	define('APPLNKDEV', 'https://github.com/rmonla/dticAulaVirtual');
+	define('APPLNKPUB', 'http://www.frlr.utn.edu.ar/dtic/aulavirtual');
 
 
+	require_once '_src/Formr/class.formr.php';
+
+	$form = new Formr\Formr();
+
+	$form->create_form('Name, Email, Comments|textarea');
 
 
 ?>
