36e13654d5465206453ccf5c95d40ba2ede24382
Ricardo MONLA <rmonla@gmail.com>
2021-03-29 16:52:15 -0300

    logVERs - Toma automáticamente el valor de versión desde la keys de logVERs.

--- _main/appInfo.php
+++ _main/appInfo.php
@@ -1,6 +1,5 @@
 <?php 
 
-    define('APPVER', '3.2.3');
     define('APPNOM', 'dtcAulaVirtual');
     define('APPAUT', 'Ricardo MONLA <rmonla@gmail.com>');
     define('APPDEV', 'https://github.com/rmonla/dticAulaVirtual');
@@ -13,17 +12,22 @@
 
       ');
 
-    $verLog = [
-      '3.2.4 => Enlaces - Saca el icono sin playlist.',
-      '3.2.3 => Enlaces - Agrega iconos de acceso a zoom y youtube.',
-      '3.2.2 => bdAulas - Cambia nombres de aulas.',
-      '3.2.1 => Ver LOGs - Actualiza lista de logs de versiones.',
-      '3.2 => bdAulas - Agrega Aula Pública Octógono.',
-      '3.1 => Actualiza registros de la bdAulas.php',
-      '3.0.3 => Optimiza y limpia código.',
-      '3.0.2 => Divide en secciones de código HTML con las funciones getHTMLs.php.',
-      '3.0.1 => Actualiza y re-genera la bdAULAs.php',
-      '3.0 => Inicia codificación de la versión 3.0'
+    $logVERs = [
+      '3.2.5' => 'logVERs - Toma automáticamente el valor de versión desde la keys de logVERs.',
+      '3.2.4' => 'Enlaces - Saca el icono sin playlist.',
+      '3.2.3' => 'Enlaces - Agrega iconos de acceso a zoom y youtube.',
+      '3.2.2' => 'bdAulas - Cambia nombres de aulas.',
+      '3.2.1' => 'logVERs - Actualiza lista de logs de versiones.',
+      '3.2' => 'bdAulas - Agrega Aula Pública Octógono.',
+      '3.1' => 'Actualiza registros de la bdAulas.php',
+      '3.0.3' => 'Optimiza y limpia código.',
+      '3.0.2' => 'Divide en secciones de código HTML con las funciones getHTMLs.php.',
+      '3.0.1' => 'Actualiza y re-genera la bdAULAs.php',
+      '3.0' => 'Inicia codificación de la versión 3.0'
     ];
+    
+    $vers = array_keys($logVERs);
+    
+    define('APPVER', $vers[0]);
 
  ?>
