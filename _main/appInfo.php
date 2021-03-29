557d4453e77ee32462f8cf87122665411980146a
Ricardo MONLA <rmonla@gmail.com>
2021-03-29 14:27:07 -0300

    3.3 => bdAulas - Cambia nombres de aulas.

--- _main/appInfo.php
+++ _main/appInfo.php
@@ -18,7 +18,7 @@
       '---3.X => Sacar los sin plailist.',
       '3.2.1 => Ver LOGs - Actualiza lista de logs de versiones.',
       '3.2 => bdAulas - Agrega Aula Pública Octógono.',
-      '3.1 => Actualiza registros de la bdAulas.php'
+      '3.1 => Actualiza registros de la bdAulas.php',
       '3.0.3 => Optimiza y limpia código.',
       '3.0.2 => Divide en secciones de código HTML con las funciones getHTMLs.php.',
       '3.0.1 => Actualiza y re-genera la bdAULAs.php',
