e162c7ccc577a4ed3248054372e5051ba6bc83e3
Ricardo MONLA <rmonla@gmail.com>
2021-03-29 16:22:15 -0300

    3.2.3 => Enlaces - Agrega iconos de acceso a zoom y youtube.

--- _main/appInfo.php
+++ _main/appInfo.php
@@ -1,7 +1,7 @@
 <?php 
 
-    define('APPVER', '3.3');
-    define('APPNOM', 'dticAulaVirtual');
+    define('APPVER', '3.2.3');
+    define('APPNOM', 'dtcAulaVirtual');
     define('APPAUT', 'Ricardo MONLA <rmonla@gmail.com>');
     define('APPDEV', 'https://github.com/rmonla/dticAulaVirtual');
     define('APPPUB', 'http://www.frlr.utn.edu.ar/dtic/aulavirtual');
@@ -14,8 +14,11 @@
       ');
 
     $verLog = [
-      '3.3 => bdAulas - Cambia nombres de aulas.',
-      '---3.X => Sacar los sin plailist.',
+      '---X.X => Sacar los sin plailist.',
+      
+
+      '3.2.3 => Enlaces - Agrega iconos de acceso a zoom y youtube.',
+      '3.2.2 => bdAulas - Cambia nombres de aulas.',
       '3.2.1 => Ver LOGs - Actualiza lista de logs de versiones.',
       '3.2 => bdAulas - Agrega Aula Pública Octógono.',
       '3.1 => Actualiza registros de la bdAulas.php',
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	include_once './_main/appInfo.php';
 	include_once './_main/bdAulas.php';
 	include_once './_main/getHTMLs.php';
-	include_once './_main/getAulas.php';
+	//include_once './_main/getAulas.php';
 
 	// include_once 'bdCals.php';
 	
