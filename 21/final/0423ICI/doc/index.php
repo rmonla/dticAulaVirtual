2f45dd70b0253419e9770d2735290d549262b1ff
Ricardo MONLA <rmonla@gmail.com>
2021-04-22 20:56:47 -0300

    Update index.php

--- 21/final/0423ICI/doc/index.php
+++ 21/final/0423ICI/doc/index.php
@@ -1,5 +1,6 @@
 <?php
-	$newURL = 'https://docs.google.com/document/d/1gJLXCfdVuxpU9t9q5AAeyJbo6KDYn28ZwbEwXNh7RwY/edit?usp=sharing';
+	$newURL = 'https://docs.google.com/document/d/1gJLXCfdVuxpU9t9q5AAeyJbo6KDYn28ZwbEwXNh7RwY/preview';
+	// $newURL = 'https://docs.google.com/document/d/1gJLXCfdVuxpU9t9q5AAeyJbo6KDYn28ZwbEwXNh7RwY/edit?usp=sharing';
 	header('Location: '.$newURL);
 	die();
 
