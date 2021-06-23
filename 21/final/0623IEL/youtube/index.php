a0472e52beb35df46575a2dc544a978cb6a5e079
Ricardo MONLA <rmonla@gmail.com>
2021-06-23 11:45:36 -0300

    Update index.php

--- 21/final/0623IEL/youtube/index.php
+++ 21/final/0623IEL/youtube/index.php
@@ -9,17 +9,17 @@
 
 			CLAVE de Transmision: 7g1r-grz5-4cp9-4tuj-9maz
 
-			URL Página: https://youtu.be/8V5MWZUyezs
+			URL Página: https://youtu.be/xY0IBot6n28
 
 		2 - Crear la reunión Zoom 
-			https://utn.zoom.us/j/82103309041
+			https://utn.zoom.us/j/88579123789
 
 		3 - Programar streaming.
 			Iniciar y dentro de la reunion ir a trasmitir personalizado.
 			
 	 */
 
-	$id = '8V5MWZUyezs';
+	$id = 'xY0IBot6n28';
 	$newURL = "https://youtu.be/$id";
 	header('Location: '.$newURL);
 	die();
