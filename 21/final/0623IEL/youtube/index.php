57a404cad8825e2634fd85c1c470e725dd4969f2
Ricardo MONLA <rmonla@gmail.com>
2021-06-23 19:01:55 -0300

    Update index.php

--- 21/final/0623IEL/youtube/index.php
+++ 21/final/0623IEL/youtube/index.php
@@ -23,7 +23,7 @@
 			TITULADOR ÁCIDO-BASE Y COLORIMÉTRICO AUTOMATIZADO   
 			»» http://www.frlr.utn.edu.ar/l/0623iel/
 
-			https://youtu.be/rshaw2-Uk2k
+			https://youtu.be/1iX3Pq2Z_2o
 
 			836 4785 8851
 			https://utn.zoom.us/j/83647858851
@@ -34,7 +34,7 @@
 
 	 */
 
-	$id = 'rshaw2-Uk2k';
+	$id = '1iX3Pq2Z_2o';
 	$newURL = "https://youtu.be/$id";
 	header('Location: '.$newURL);
 	die();
