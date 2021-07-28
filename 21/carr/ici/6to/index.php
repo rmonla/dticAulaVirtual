9b5b8666e403afd6620bba05faf4482e896ec008
Ricardo MONLA <rmonla@gmail.com>
2021-07-28 18:20:33 -0300

    Update index.php

--- 21/carr/ici/6to/index.php
+++ 21/carr/ici/6to/index.php
@@ -2,27 +2,7 @@
 /* «® INCLUIDES  ®» */
 	include_once "../../../../_main/bdAulas.php";
 
-/** «® MAIN  ®» **/	
-	$idA = 'ICI.6to.21';
-	$dst = getAulaDat($idA, 'aUrlZ');
-	$tit = getAulaDat($idA, 'aNOM');
-	$dsc = "Acceso al Aula Virtual $idA - UTNLaRioja 2021";
-	$tmp = 1; 
-	$img = AV_IMGS."logoUTNLR-Mini.png";
-
 /** «® SALIDA  ®» **/
-	echo <<<HTML
-
-	<html>
-		<head>
-			<title>$tit</title>
-			<meta property="og:title"  content="$tit">
-			<meta name="description"   content="$dsc">
-			<meta property="og:image"  content="$img">
-			<meta http-equiv="Refresh" content="$tmp;url=$dst"/>
-		</head>
-	</html>		
-
-HTML;
+	echo getAulaDat('ICI.6to.21', 'aGoTo');
 
 ?>
