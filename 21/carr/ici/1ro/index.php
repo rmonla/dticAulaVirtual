f52084eb00147733f46f5b419b4b926bd45555b4
Ricardo MONLA <rmonla@gmail.com>
2021-07-28 18:07:22 -0300

    Update index.php

--- 21/carr/ici/1ro/index.php
+++ 21/carr/ici/1ro/index.php
@@ -1,29 +1,29 @@
 <?php  
 /* «® INCLUIDES  ®» */
 	include_once "../../../../_main/bdAulas.php";
-	include_once "../../../../appINCs.php";
+	//include_once "../../../../appINCs.php";
 
-/** «® MAIN  ®» **/	
-	$idA = 'ICI.1ro.21';
-	$dst = getAulaDat($idA, 'aUrlZ');
-	$tit = getAulaDat($idA, 'aNOM');
-	$dsc = "Acceso al Aula Virtual $idA";
-	$tmp = 1; 
-	$img = AV_IMGS."logoUTNLR-Mini.jpg";
+// /** «® MAIN  ®» **/	
+// 	$idA = 'ICI.1ro.21';
+// 	$tit = getAulaDat($idA, 'aNOM');
+// 	$dsc = "Acceso al Aula Virtual $idA";
+// 	$tmp = 1; 
+// 	$img = AV_IMGS."logoUTNLR-Mini.jpg";
 
 /** «® SALIDA  ®» **/
-	echo <<<HTML
+	echo getAulaDat('ICI.1ro.21', 'aGoTo');
+// 	echo <<<HTML
 
-	<html>
-		<head>
-			<title>$tit</title>
-			<meta property="og:title"  content="$tit">
-			<meta name="description"   content="$dsc">
-			<meta property="og:image"  content="$img">
-			<meta http-equiv="Refresh" content="$tmp;url=$dst"/>
-		</head>
-	</html>		
+// 	<html>
+// 		<head>
+// 			<title>$tit</title>
+// 			<meta property="og:title"  content="$tit">
+// 			<meta name="description"   content="$dsc">
+// 			<meta property="og:image"  content="$img">
+// 			<meta http-equiv="Refresh" content="$tmp;url=$dst"/>
+// 		</head>
+// 	</html>		
 
-HTML;
+// HTML;
 
 ?>
