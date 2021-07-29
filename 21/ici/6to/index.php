0b2a290efd4dd442dc30ca908be3aa67d4a38cbf
Ricardo MONLA <rmonla@gmail.com>
2021-07-28 22:39:22 -0300

    Update index.php

--- 21/ici/6to/index.php
+++ 21/ici/6to/index.php
@@ -2,6 +2,6 @@
 /* «® INCLUIDES  ®» */
 	include_once "../../../_main/bdAulas.php";
 /** «® SALIDA  ®» **/
-	//echo getAulaDat('ICI.6to.21', 'aGoTo');
-	exit(var_dump($bdAs));
+	echo getAulaDat('ICI.6to.21', 'aGoTo');
+	//exit(var_dump($bdAs));
 ?>
