239983bd88f4234b4a5484444dbb9a4356b6d38b
Ricardo MONLA <rmonla@gmail.com>
2021-08-19 21:41:36 -0300

    Actu-210819_2141

---
+++ 21/gaia/mensajes.php
@@ -0,0 +1,21 @@
+_*INFORMACIÓN DEL EVENTO*_
+_GAIA.0819_
+TECNOLOGÍA DE MITIGACIÓN EN RESIDUOS PATÓGENOS Y DESARROLLO DE DISPOSICIÓN FINAL EN FORMA SUSTENTABLE        
+Sebastián BENÍTEZ HERMIDA
+_[19/08/21 20Hs]_
+
+[ 19:30 ]* - Previa, Preparación y Testeos.
+[ 20:00 ]* - Inicia Transmisión: Ricardo MONLA.
+[ 20:10 ]* - Moderador: Diego BARTOLOME.
+[ 20:15 ]* - Exposición: Sebastián BENÍTEZ.
+
+Para ingresar lo hacemos en la opción de ZOOM del enlace:
+
+http://www.frlr.utn.edu.ar/dtic/aulavirtual/21/gaia/confs/
+
+
+_*CONFERENCIAS ANTERIORES*_
+»» https://www.youtube.com/playlist?list=PLSOY8GuPHc0BoPqexDU39iWkFklN_WJC2
+
+_*ACREDITACIÓN y ASISTENCIA*_ 
+»» https://forms.gle/7iSuZhxSW6xx65JN8
